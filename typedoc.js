e95810bbb68baf554b07b072eb8c273c396ad466
ian <ian@cryptape.com>
2024-01-05 13:02:14 +0800

    cleanup typedoc options

--- typedoc.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('typedoc').TypeDocOptions} */
-module.exports = {
-  entryPoints: ["packages/cobuild"],
-  entryPointStrategy: "packages",
-  plugin: ["typedoc-plugin-missing-exports"]
-  out: "apps/docs/public/api/",
-};
