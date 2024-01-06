bceae63ffd41d948559bd7cf3b25258681c5c8f6
ian <ian@cryptape.com>
2024-01-07 04:18:31 +0800

    chore: fix imports order

--- packages/cobuild/src/index.ts
+++ packages/cobuild/src/index.ts
@@ -1,10 +1,10 @@
 import { blockchain } from "@ckb-lumos/base";
 import { number } from "@ckb-lumos/codec";
 
+const { Uint32LE } = number;
+const { Bytes, Byte32, BytesVec, CellOutputVec } = blockchain;
+
 export * from "./building-packet";
 export * as factory from "./factory";
 export * from "./witness-layout";
 export { Byte32, Bytes, BytesVec, CellOutputVec, Uint32LE };
-
-const { Uint32LE } = number;
-const { Bytes, Byte32, BytesVec, CellOutputVec } = blockchain;
